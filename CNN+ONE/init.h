b75f6dd80b4917f31a871699c562e90939698446
kaharjan <kaharjan21@gmail.com>
2016-11-21 15:23:36 +0800

    first time

--- CNN+ONE/init.h
+++ CNN+ONE/init.h
@@ -138,9 +138,9 @@ void init() {
 		string tail_s = (string)(buffer);
 		fscanf(f,"%s",buffer);
 		//bags_train["m.0ycvs\tm.02pxj3t\tcontatins"]=<0>
-		fprint(logg, "headList.size()= %d", headList.size());
+		fprintf(logg, "headList.size()= %d", headList.size());
 		bags_train[e1+"\t"+e2+"\t"+(string)(buffer)].push_back(headList.size());
-		//fprint(logg, "bags_train[e1+e2+(string)(buffer)]= %d", bags_train[e1 + "\t" + e2 + "\t" + (string)(buffer)]);
+		fprintf(logg, "bags_train[e1+e2+(string)(buffer)]= %d", bags_train[e1 + "\t" + e2 + "\t" + (string)(buffer)]);
 		//relation id
 		int num = relationMapping[(string)(buffer)];
 		int len = 0, lefnum = 0, rignum = 0;
